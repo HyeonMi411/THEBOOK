f45f43dc203b1547e0fb950a5e108caf9c63adcf
HyeonMi411 <jewelkhm@naver.com>
2026-09-23 12:11:41 +0900

    Delete project_5 directory

--- project_5/notices/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.NoticeListCreateView.as_view()),
-    path("search", views.search_notices),
-    path("<int:pk>", views.NoticeDetailView.as_view()),
-]
